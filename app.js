53abe098c504cc5026789f873daba4c1c552e987
beejeybrown78 <48865452+beejeybrown78@users.noreply.github.com>
2023-02-02 13:54:32 +0200

    Activebox

---
+++ app.js
@@ -0,0 +1,61 @@
+$(function() {
+    /* FIXED ШАПКА*/
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll  resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    /*Smooth scroll*/
+
+    $("[data-scroll").on("click", function(event) {
+        event.preventDefault();
+        let elementId = $(this).data("scroll");
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+        console.log(elementOffset);
+
+        $("html, body").animate({
+                scrollTop: elementOffset - 80,
+            },
+            700
+        );
+    });
+
+    /*nav Toggle*/
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+        nav.toggleClass("show");
+    });
+
+    /*Testimonials*/
+
+    let slider = $("#testimonialsslider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        arrows: false,
+    });
+});
